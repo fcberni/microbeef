438c1fe427e95eea2aa9ee25347bad40d08564d1
Frank <fcberni@hotmail.com>
2020-12-18 18:00:52 -0500

    Event listeners and Dyanmic charts

--- D3sample/plot.js
+++ D3sample/plot.js
@@ -1,3 +1,37 @@
+// d3.json("samples.json").then(function(data){
+//     console.log("hello");
+// });
+
+// loading the data from samples.json
 d3.json("samples.json").then(function(data){
-    console.log("hello");
+    console.log(data);
+});
+
+//Extracting only wfreq
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person => person.wfreq);
+//     console.log(wfreq);
+// });
+
+// Same as above but now in desc order
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     console.log(wfreq);
+// });
+
+//We want to also remove nulls
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+// Using Object.entires and forEach methods
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
 });
