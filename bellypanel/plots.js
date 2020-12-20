20bfa72ac790d1af0a3ca4640f10c4d58121c359
Frank <fcberni@hotmail.com>
2020-12-20 17:54:32 -0500

    final coding

---
+++ bellypanel/plots.js
@@ -0,0 +1,41 @@
+
+// function for the dropdown menu and reading JSON data
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+//
+function optionChanged(newSample) {
+  console.log(newSample);
+}
+
+// print information to the Demographic Info panel
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+// 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.location);
+  });
+}
